232ce68d5a61e9679edd246e0d2cd2ac023ecf96
unknown <som.o.pichaya@gmail.com>
2026-04-09 23:50:17 +0700

    Modify player movement to have sprint ability.

--- Assets/Scripts/PlayerMovement3D.cs
+++ Assets/Scripts/PlayerMovement3D.cs
@@ -3,27 +3,39 @@ using UnityEngine;
 public class PlayerMovement3D : MonoBehaviour
 {
     public float speed = 5f;
+    public float currentSpeed;
 
+    
     private void Update()
     {
         if (Input.GetKey(KeyCode.W)) 
         {
-            transform.position += new Vector3(0f, 0f, 1f) * speed * Time.deltaTime;
+            transform.position += new Vector3(0f, 0f, 1f) * currentSpeed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-1f, 0f, 0f) * speed * Time.deltaTime;
+            transform.position += new Vector3(-1f, 0f, 0f) * currentSpeed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(0f, 0f, -1f) * speed * Time.deltaTime;
+            transform.position += new Vector3(0f, 0f, -1f) * currentSpeed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(1f, 0f, 0f) * speed * Time.deltaTime;
+            transform.position += new Vector3(1f, 0f, 0f) * currentSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = speed * 2f;
+
+        }
+        else
+        {
+            currentSpeed = speed;
         }
     }
 }
