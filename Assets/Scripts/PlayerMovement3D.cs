71632170f2742747893010ac0df4aa2f391e9baa
unknown <som.o.pichaya@gmail.com>
2026-04-10 13:33:18 +0700

    Office: Incease speed

--- Assets/Scripts/PlayerMovement3D.cs
+++ Assets/Scripts/PlayerMovement3D.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement3D : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 10f;   //Office buff speed
     public float currentSpeed;
 
     
